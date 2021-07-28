60eec73a80a4003be2acadada0070cd77cad4c25
Yasmin <yasmin@yasmin.so>
2021-07-28 19:03:06 +0800

    Organized Code

--- Program.cs
+++ Program.cs
@@ -9,30 +9,29 @@ namespace CarRandomizer
     {
         static void Main(string[] args)
         {
-            
-            var CarGUID = File.ReadAllLines(@"Car GUID.txt");
-            
-            Random rand = new Random();  
-            //adding file path
-            string orgXml = args[0];
-            // Option1: Using SetAttributeValue()
-            XDocument xmlDoc = XDocument.Load(orgXml);  
-            // Update Element value  
-            var items = from item in xmlDoc.Descendants("LibraryObject")  
-                        select item;  
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please put path to XML file as the First Argument");
+                return;
+            }
 
-            foreach (XElement itemElement in items)  
-            {  
-                var currentName =itemElement.Attribute("name").Value;
-                if (CarGUID.Contains(currentName.TrimStart('{').TrimEnd('}'))) {
-                int index = rand.Next(CarGUID.Length);  
-                itemElement.SetAttributeValue("name", "{"+CarGUID[index]+"}");  
+            var carGuid = File.ReadAllLines(@"Car GUID.txt");
+            string orgXml = args[0];
+            Random rand = new Random();
+
+            XDocument xmlDoc = XDocument.Load(orgXml);
+
+            foreach (XElement libraryObject in xmlDoc.Descendants("LibraryObject"))
+            {
+                var currentName = libraryObject.Attribute("name").Value;
+                if (carGuid.Contains(currentName.TrimStart('{').TrimEnd('}')))
+                {
+                    int index = rand.Next(carGuid.Length);
+                    libraryObject.SetAttributeValue("name", "{" + carGuid[index] + "}");
                 }
-                
-            }  
-            
-            xmlDoc.Save(args[0]);  
-            Console.WriteLine(); 
+            }
+
+            xmlDoc.Save(args[0]);
 
         }
     }
